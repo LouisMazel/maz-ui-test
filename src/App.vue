fd5f30de7e19aa77b910d644b74bea2acdc986c6
Mazel <mazuel.loic@gmail.com>
2021-03-08 21:30:07 +0100

    feat(maz-ui): test import of root css vars

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <br />
+    <br />
+
+    <!-- <MazGallery :images="[]" /> -->
+
+    <br />
+    <br />
+
+    <MazBtn>
+      Primary Button
+    </MazBtn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      value: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/plugins/maz-ui/index.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import { MazBtn, MazGallery } from "maz-ui";
+import "./../../assets/_root-variables.css";
+
+Vue.use(MazBtn);
+Vue.use(MazGallery);
